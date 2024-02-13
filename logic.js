09ca87805ab5a5b46c2df93be4cbc23989e8cd71
gtpWebDev <33732289+gtpWebDev@users.noreply.github.com>
2024-02-13 12:57:26 +0000

    Game logic improvements
    Add DOM updates for player and computer scores.
    Improve result messaging to make each round result clearer.
    Add restart game capability.

--- logic.js
+++ logic.js
@@ -6,139 +6,15 @@
 let computerScore = 0
 let playerScore = 0
 
+// Prepare DOM updates for score updates, and generate page button functionality
 let resultDisplay = document.querySelector("#resultDisplay")
-let newParagraph = document.createElement("p")
+let resultParagraph = document.createElement("p")
+let playerScoreDisplay = document.querySelector("#playerScore")
+let playerScoreParagraph = document.createElement("p")
+let computerScoreDisplay = document.querySelector("#computerScore")
+let computerScoreParagraph = document.createElement("p")
+
 setupGameButtons()
-
-function getComputerChoice() {
-
-  // Returns one of "Rock, "Paper", "Scissors" with equal probability
-
-  let intRandom = Math.floor(3 * Math.random())
-
-  switch (intRandom) {
-    case 0:
-      return "rock"
-      break;
-    case 1:
-      return "paper"
-      break
-    case 2:
-      return "scissors"
-      break
-    default:
-      return "ERROR!"
-  }
-
-}
-
-function resetGame() {
-  playerScore = 0
-  computerScore = 0
-}
-
-function addResultToDisplay(message,playerScore,computerScore) {
-  
-  let latestScoreMessage = `Latest score. Player: ${playerScore}, Computer: ${computerScore}`
-  
-  let endGameMessage = ""
-
-  if (playerScore === 5 || computerScore === 5) {
-    if (playerScore === 5) {
-      endGameMessage = "Player wins."
-    } else if (computerScore === 5) {
-      endGameMessage = "Computer wins."
-    }
-    resetGame()
-  }
-
-
-  newParagraph.textContent = message + ". " + latestScoreMessage + ". " + endGameMessage + "."
-  resultDisplay.appendChild(newParagraph)
-}
-
-
-function playRound(playerInput) {
-
-  let playerSelection = playerInput.toLowerCase()
-  let computerSelection = getComputerChoice()
-
-  let message = ""
-
-  if (playerSelection === computerSelection) {
-    message = `No winner! You both chose ${playerSelection}`
-
-  } else if (playerSelection === "rock") {
-    if (computerSelection === "paper") {
-      message = "You Lose! Paper beats Rock"
-      computerScore++
-    } else if (computerSelection === "scissors") {
-      message = "You Win! Rock beats Scissors"
-      playerScore++
-    }
-
-  } else if (playerSelection === "paper") {
-    if (computerSelection === "rock") {
-      message = "You Win! Paper beats Rock"
-      playerScore++
-    } else if (computerSelection === "scissors") {
-      message = "You Lose! Scissors beat Paper"
-      computerScore++
-    }
-
-  } else if (playerSelection === "scissors") {
-    if (computerSelection === "paper") {
-      message = "You Win! Scissors beat Paper"
-      playerScore++
-    } else if (computerSelection === "rock") {
-      message = "You Lose! Rock beats Scissors"
-      computerScore++
-    }
-
-  }
-
-  addResultToDisplay(message,playerScore,computerScore)
-
-}
-
-function reportGameResult(playerWinCount,computerWinCount) {
-
-  if (playerWinCount === computerWinCount) {
-    return "Game drawn!"
-  } else if (playerWinCount > computerWinCount) {
-    return "Player wins!"
-  } else {
-    return "Computer wins!"
-  }
-
-}
-
-
-function playGame() {
-
-  let playerWinCount = 0
-  let computerWinCount = 0
-
-
-    // Collect player and computer choices
-    let playerSelection = prompt("Please enter any of \"Rock\", \"Paper\" or \"Scissors\"")
-    console.log(`Player choice is ${playerSelection}.`)
-    let computerSelection = getComputerChoice()
-    console.log(`Computer choice is ${computerSelection}.`)
-
-    // Decide who wins round
-    let = roundResult = playRound(playerSelection,computerSelection)
-    if (roundResult === "Player Wins") {
-      playerWinCount++
-    } else if (roundResult === "Computer Wins") {
-      computerWinCount++
-    }
-    console.log(`Standing after round ${round} is Player: ${playerWinCount}, Computer: ${computerWinCount}`)
-
-
-  console.log(reportGameResult(playerWinCount,computerWinCount))
-
-}
 
 function setupGameButtons() {
 
@@ -149,4 +25,113 @@ function setupGameButtons() {
     })
   })
 
+  const restartGameButton = document.querySelector("#restartButton")
+  restartGameButton.addEventListener("click",resetGame)
+
 }
+
+
+function playRound(playerSelection) {
+
+  playerSelection = playerSelection.toLowerCase()
+  let computerSelection = getComputerChoice()
+
+  let resultMessage = ""
+
+  let loseMessage = `You chose ${playerSelection}. Computer chose ${computerSelection}. You lose!`
+  let winMessage = `You chose ${playerSelection}. Computer chose ${computerSelection}. You win!`
+
+  if (playerSelection === computerSelection) {
+    resultMessage = `No winner! You both chose ${playerSelection}.`
+
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "paper") {
+      resultMessage = loseMessage
+      computerScore++
+    } else if (computerSelection === "scissors") {
+      resultMessage = winMessage
+      playerScore++
+    }
+
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      resultMessage = winMessage
+      playerScore++
+    } else if (computerSelection === "scissors") {
+      resultMessage = loseMessage
+      computerScore++
+    }
+
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "paper") {
+      resultMessage = winMessage
+      playerScore++
+    } else if (computerSelection === "rock") {
+      resultMessage = loseMessage
+      computerScore++
+    }
+
+  }
+
+  constructResultMessage(resultMessage,playerScore,computerScore)
+  updateScoreDisplays(playerScore,computerScore)
+
+}
+
+function updateScoreDisplays(playerScore,computerScore) {
+
+  playerScoreParagraph.textContent = playerScore
+  playerScoreDisplay.appendChild(playerScoreParagraph)
+  computerScoreParagraph.textContent = computerScore
+  computerScoreDisplay.appendChild(computerScoreParagraph)
+
+}
+
+function updateResultDisplay(resultMessage) {
+  resultParagraph.textContent = resultMessage
+  resultDisplay.appendChild(resultParagraph)
+
+}
+
+function constructResultMessage(resultMessage,playerScore,computerScore) {
+   
+  let endGameMessage = ""
+
+  if (playerScore === 5 || computerScore === 5) {
+    if (playerScore === 5) {
+      endGameMessage = " Player wins."
+    } else if (computerScore === 5) {
+      endGameMessage = " Computer wins."
+    }
+  }
+
+  updateResultDisplay(resultMessage.concat(endGameMessage))
+}
+
+
+function getComputerChoice() {
+
+  // Returns one of "Rock, "Paper", "Scissors" with equal probability
+
+  switch (Math.floor(3 * Math.random())) {
+    case 0:
+      return "rock"
+    case 1:
+      return "paper"
+    case 2:
+      return "scissors"
+    default:
+      return "ERROR!"
+  }
+
+}
+
+
+function resetGame() {
+  playerScore = 0
+  computerScore = 0
+  updateScoreDisplays(0,0)
+  updateResultDisplay("")
+}
+
+
